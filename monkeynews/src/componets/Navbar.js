584dd1f1434019011798d6ab5402f43e68d413b3
ganesh <ganeshparjaptm5@gmail.com>
2023-02-24 21:37:21 +0530

    add page and reande api etc

--- monkeynews/src/componets/Navbar.js
+++ monkeynews/src/componets/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             Monkey
