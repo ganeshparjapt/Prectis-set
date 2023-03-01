0554f8e4a3fa9c8e7f27481bbe70bbb00e919294
ganesh <ganeshparjaptm5@gmail.com>
2023-03-01 18:27:35 +0530

    add function to handle all buttons

--- my-textediter/src/compontes/Form.js
+++ my-textediter/src/compontes/Form.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+const [textArea , settextArea] = useState("");
+  let  handleOnchange = (event) =>{
+    let newTextArea = (event.target.value);
+    settextArea(newTextArea)
+    }
+    let handleUpperCase = () =>{
+        let newtext = (textArea.toUpperCase());
+        settextArea(newtext);
+    }
+    let handleLoverCase = () =>{
+        let newtext = (textArea.toLocaleLowerCase());
+        settextArea(newtext);
+    }
+    let removeSpace = () =>{
+        // let newtext =(textArea.replace(/\s+/g,' '));
+        let newtext = (textArea.split(/[ ]+/).join(" "));
+        settextArea(newtext);
+     }
+    let copyText = () =>{
+            navigator.clipboard.writeText(textArea);
+            alert("copied the text" );
+    }
+    const clear = () =>{
+        let newtext =("");
+        settextArea(newtext);
+    }
+    const capitalfirst = () =>{
+       let newtext = textArea.split(" ");
+       for(let i=0; i< newtext.length; i++){
+        newtext[i] = newtext[i].charAt(0).toUpperCase() + newtext[i].slice(1);
+        let newtext1 = newtext.join(" ");
+        settextArea(newtext1);
+       }
+       settextArea(newtext);
+    }
+
+
   return (
     <>
       <div className="container" style={{backgroundColor: "white"}}>
         <div className=" mb-3">
-          <label for="exampleFormControlInput1" className="form-label">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
             Email address
           </label>
           <input
@@ -23,8 +60,20 @@ export default function Form() {
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="6"
+            onChange={handleOnchange}
+           
+            value={textArea}
           ></textarea>
+        
+        <button type="button" className="btn btn-primary" style={{marginRight: "1rem", marginTop: "1rem"}} onClick={handleUpperCase}>To Upper case</button>
+        <button type="button" className="btn btn-primary" style={{marginRight: "1rem", marginTop: "1rem"}} onClick={handleLoverCase}>To Lover Case</button>
+        <button type="button" className="btn btn-primary" style={{marginRight: "1rem", marginTop: "1rem"}} onClick={removeSpace}>Remove Space</button>
+        <button type="button" className="btn btn-primary" style={{marginRight: "1rem", marginTop: "1rem"}} onClick={copyText}>Copy Text</button>
+        <button type="button" className="btn btn-primary" style={{marginRight: "1rem", marginTop: "1rem"}} onClick={clear}>Clear</button>
+        <button type="button" className="btn btn-primary" style={{marginRight: "1rem", marginTop: "1rem"}} onClick={capitalfirst}>First capital</button>
+
         </div>
+        <h2>preview hear</h2>
       </div>
     </>
   );
--- my-textediter/src/compontes/Navbar.js
+++ my-textediter/src/compontes/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             {props.title}
           </a>
           <button
@@ -22,18 +22,18 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/">
                   Link
                 </a>
               </li>
 
               <li className="nav-item">
-                <a className="nav-link ">Disabled</a>
+                <a className="nav-link" href="/">Disabled</a>
               </li>
             </ul>
           </div>
